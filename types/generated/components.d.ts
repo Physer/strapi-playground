4f11fef6bf9111a991e2e4bb994ba7b049c17148
Alex Schouls <alex_schouls@live.com>
2024-11-25 21:35:46 +0100

    adding carousel

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,45 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface CarouselCarousel extends Struct.ComponentSchema {
+  collectionName: 'components_carousel_carousels';
+  info: {
+    description: '';
+    displayName: 'Carousel';
+    icon: 'slideshow';
+  };
+  attributes: {
+    CarouselItems: Schema.Attribute.Component<'carousel.carousel-item', true>;
+    CarouselSlider: Schema.Attribute.Component<'carousel.carousel-text', true>;
+    Title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface CarouselCarouselItem extends Struct.ComponentSchema {
+  collectionName: 'components_carousel_carousel_items';
+  info: {
+    displayName: 'CarouselItem';
+    icon: 'bulletList';
+  };
+  attributes: {
+    Description: Schema.Attribute.Text & Schema.Attribute.Required;
+    Image: Schema.Attribute.Media<'images' | 'files'> &
+      Schema.Attribute.Required;
+    Title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface CarouselCarouselText extends Struct.ComponentSchema {
+  collectionName: 'components_carousel_carousel_texts';
+  info: {
+    description: '';
+    displayName: 'CarouselText';
+    icon: 'pencil';
+  };
+  attributes: {
+    Text: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
 export interface CustomHero extends Struct.ComponentSchema {
   collectionName: 'components_custom_heroes';
   info: {
@@ -17,6 +57,9 @@ export interface CustomHero extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'carousel.carousel': CarouselCarousel;
+      'carousel.carousel-item': CarouselCarouselItem;
+      'carousel.carousel-text': CarouselCarouselText;
       'custom.hero': CustomHero;
     }
   }
