7665c63749491173d3dae2b6447d957ce0241e6e
Alex Schouls <alex_schouls@live.com>
2024-11-25 21:28:39 +0100

    added Hero component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,23 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface CustomHero extends Struct.ComponentSchema {
+  collectionName: 'components_custom_heroes';
+  info: {
+    displayName: 'Hero';
+    icon: 'picture';
+  };
+  attributes: {
+    BackgroundImage: Schema.Attribute.Media<'images' | 'files'> &
+      Schema.Attribute.Required;
+    MainImage: Schema.Attribute.Media<'images' | 'files'>;
+    Name: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'custom.hero': CustomHero;
+    }
+  }
+}
