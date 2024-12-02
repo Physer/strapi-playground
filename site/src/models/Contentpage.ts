1e120f25156511a3fc8652c92ceec98cb07d0791
Alex Schouls <alex_schouls@live.com>
2024-12-02 14:08:15 +0100

    added contentpages from CMS with dynamic routes to site

--- site/src/models/Contentpage.ts
+++ site/src/models/Contentpage.ts
@@ -1,12 +1,14 @@
 import type { RichText } from './RichText';
+import type { Image } from './Image';
 import type { StrapiData, StrapiResponse } from './StrapiResponse';
 
 export interface Contentpage extends StrapiResponse {
     data: Array<ContentpageData>;
 }
 
-interface ContentpageData extends StrapiData {
+export interface ContentpageData extends StrapiData {
     header: string;
     content: Array<RichText>;
+    contacts: Array<Image>;
     url: string;
 }
